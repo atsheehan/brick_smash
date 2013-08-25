917d021ba1da85eb3e019567d403c67ad165f57e
Adam Sheehan <a.t.sheehan@gmail.com>
2013-08-25 16:11:20 -0400

    Add Image model.

---
+++ src/main/java/com/bazbatlabs/smashballs/models/Image.java
@@ -0,0 +1,16 @@
+package com.bazbatlabs.smashballs.models;
+
+public final class Image {
+
+    public final float x;
+    public final float y;
+    public final float w;
+    public final float h;
+
+    public Image(float x, float y, float w, float h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+}
